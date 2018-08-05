98c22930d7ff8f446f464ff18e2ad8474cdeb60c
tranerius <36367029+tranerius@users.noreply.github.com>
2018-08-05 17:45:36 +0200

    add

--- sketch_aug05a/sketch_aug05a.ino
+++ sketch_aug05a/sketch_aug05a.ino
@@ -10,53 +10,22 @@ void setup() {
 
 long time = 0;
 bool ON = true;
+const byte LED_NUM = 5;
 byte LED_COUNT = 0;
+
+void LEDControl(){
+  if (LED_COUNT == LED_NUM){
+    if(ON) ON = false;
+    else ON = true;
+    LED_COUNT = 0;
+  }
+  if(ON) digitalWrite((LED_COUNT++)+8,HIGH);
+  else digitalWrite((LED_COUNT++)+8,LOW);
+}
 
 void loop() {
   if((millis()-time)>=50 && !digitalRead(7)){
-    switch(LED_COUNT){
-      case 5:
-        if(ON){
-          Serial.println("ON");
-          ON = false;
-          LED_COUNT = 0;
-        }
-        else {
-          Serial.println("OFF");
-          ON = true;
-          LED_COUNT = 0;
-        }
-      case 0:
-        Serial.println("1");
-        if(ON) digitalWrite(8,HIGH);
-        else digitalWrite(8,LOW);
-        ++LED_COUNT;
-        break;
-      case 1:
-        Serial.println("2");
-        if(ON) digitalWrite(9,HIGH);
-        else digitalWrite(9,LOW);
-        ++LED_COUNT;
-        break;
-      case 2:
-        Serial.println("3");
-        if(ON) digitalWrite(10,HIGH);
-        else digitalWrite(10,LOW);
-        ++LED_COUNT;
-        break; 
-      case 3:
-        Serial.println("4");
-        if(ON) digitalWrite(11,HIGH);
-        else digitalWrite(11,LOW);
-        ++LED_COUNT;
-        break; 
-      case 4:
-        Serial.println("5");
-        if(ON) digitalWrite(12,HIGH);
-        else digitalWrite(12,LOW);
-        ++LED_COUNT;
-        break;
-    }
+    LEDControl();
     while(!digitalRead(7));
     time = millis();
   }
